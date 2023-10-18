899205f1d670c72c13b932f99f72757ee633117e
Mayangmayum Ashish <81023786+Ashish543-tech@users.noreply.github.com>
2023-10-18 12:09:11 +0530

    Time: 0 ms (100%), Space: 40.7 MB (10.86%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        HashMap<Character, String> map = new HashMap<>();
+        
+        if(words.length!=pattern.length()){
+            return false;
+        }
+
+        for(int i=0; i<pattern.length(); i++){
+
+            if(map.containsKey(pattern.charAt(i))){
+                if(!map.get(pattern.charAt(i)).equals(words[i])){
+                    return false;
+                }
+            }
+            else{
+                if(map.containsValue(words[i])){
+                    return false;
+                }
+                map.put(pattern.charAt(i), words[i]);
+            }
+        }
+        return true;
+    }
+}
