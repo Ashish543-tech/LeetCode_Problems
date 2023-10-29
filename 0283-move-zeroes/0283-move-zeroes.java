fa356ecaa1f20bef14ae411d3a6a0ed3f2231e4e
Mayangmayum Ashish <81023786+Ashish543-tech@users.noreply.github.com>
2023-10-29 10:49:18 +0530

    Time: 3 ms (23.37%), Space: 45.1 MB (33.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int i=0;
-       int j=1;
-       while(i < nums.length && j <nums.length){
-           if(nums[i] == 0 && nums[j] !=0){
-               nums[i] = nums[i] ^ nums[j];
-               nums[j] = nums[i] ^ nums[j];
-               nums[i] = nums[i] ^ nums[j];
-               i++;
-               j++;
-           }
-           else if(nums[i] == 0 && nums[j] == 0){
-               j++;
-           }
-           else{
-               i++;
-               j++;
-           }
-       } 
+       int j = -1;
+       for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+
+        if(j != -1){
+            for(int i=j+1; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[j] = nums[j]^nums[i];
+                nums[i] = nums[j]^nums[i];
+                nums[j] = nums[j]^nums[i];
+                j++;
+            }
+        }
+        } 
     }
 }
